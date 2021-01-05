855be9e7587e9599d7d7efe4741c584c5d22d317
marco cheung <marco.cheung@diginex.com>
2021-01-05 09:03:53 +0800

    howto

---
+++ src/index.js
@@ -0,0 +1,57 @@
+
+const fs = require('fs');
+const path = require('path');
+const Subscription = require('./resolvers/Subscription')
+const { PrismaClient } = require('@prisma/client')
+const { ApolloServer, PubSub } = require('apollo-server');
+const pubsub = new PubSub()
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
+  },
+  Mutation: {
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
+  },
+  Subscription
+  // User,
+  // Link
+}
+
+const prisma = new PrismaClient()
+// 3
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
